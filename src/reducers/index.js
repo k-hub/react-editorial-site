13795787ab2bc93a0145a1fb7982eb0b7d418518
Kathy <kathyy.ma@gmail.com>
2018-06-14 09:34:46 -0700

    Style fix

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
 import PostsReducer from './posts_reducer';
+
 const rootReducer = combineReducers({
-    posts:PostsReducer
+    posts: PostsReducer,
     //state: (state = {}) => state
 });
 
